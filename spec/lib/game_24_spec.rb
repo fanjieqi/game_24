0604fa8e2267f962e85ee05161d52e1d8e7a910f
范捷琦 <jieqi.fan@corp.elong.com>
2017-02-27 22:39:32 +0800

    add the game_24_spec.rb

---
+++ spec/lib/game_24_spec.rb
@@ -0,0 +1,24 @@
+require "game_24"
+
+RSpec.describe Game24, :type => :lib do
+  it "generates the answers of [3, 3, 6, 6]" do
+    answers = Game24.new(array: [3, 3, 6, 6]).work
+    expect(answers.size).to eq(1)
+    answers.each do |answer|
+      expect(eval(answer)).to eq(24.0)
+    end
+  end
+
+  it "generates the answers of [3 2 3 4]" do
+    answers = Game24.new(array: [3, 2, 3, 4]).work
+    expect(answers.size).to eq(0)
+  end
+
+  it "generates the answers of [5, 5, 6, 6]" do
+    answers = Game24.new(array: [5, 5, 6, 6]).work
+    expect(answers.size).to eq(5)
+    answers.each do |answer|
+      expect(eval(answer)).to eq(24.0)
+    end
+  end
+end
