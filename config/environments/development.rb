40543603d40705a16a4a8c79595d111e7522ace4
范捷琦 <jieqi.fan@corp.elong.com>
2017-03-01 10:48:47 +0800

    add the config.action_view.logger and config.assets.digest

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,10 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.action_view.logger = nil
+
+  config.assets.digest = false
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
